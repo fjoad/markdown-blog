54233badf8d8ac2f4ed8879e9bad43af4618cab1
fjoad <faaizjoad1@gmail.com>
2021-01-09 08:51:05 +0300

    added slugify and markdown functionality

--- routes/articles.js
+++ routes/articles.js
@@ -13,9 +13,9 @@ router.get('/new', (req, res) => {
 })
 
 //view specific article 
-router.get('/:id', async (req, res) => {
+router.get('/:slug', async (req, res) => {
     try {
-        const article = await Article.findById(req.params.id)
+        const article = await Article.findOne({ slug: req.params.slug })
         res.render('articles/show', {article, article})
     } catch (error) {
         console.error(error)
@@ -23,21 +23,43 @@ router.get('/:id', async (req, res) => {
     }
 })
 
-//create new article 
-router.post('/', async (req, res) => {
-    let article = new Article({
-        title: req.body.title, 
-        description: req.body.description,
-        markdown: req.body.markdown,
-        createdAt: new Date()
-    })
-    try {
-        article = await article.save()
-        res.redirect(`/articles/${article.id}`)
-    } catch (err) {
-        console.error(err)
-        res.render('articles/new', {article: article})
-    }
+//edit articles form 
+router.get('/edit/:id', async (req, res) => {
+    const article = await Article.findById(req.params.id)
+    res.render('articles/edit', {article: article})
 })
+
+//create new article 
+router.post('/', async (req, res, next) => {
+    req.article = new Article()
+    next()
+}, saveArticleAndRedirect('new'))
+
+//PUT (edit) article 
+router.put('/:id', async (req, res, next) => {
+    req.article = await Article.findById(req.params.id)
+    next()
+}, saveArticleAndRedirect('edit'))
+
+//delete article 
+router.delete('/:id', async (req, res) => {
+    await Article.findByIdAndDelete(req.params.id)
+    res.redirect('/')
+})
+
+function saveArticleAndRedirect(path) {
+    return async (req, res) => {
+        let article = req.article
+        article.title = req.body.title
+        article.description = req.body.description
+        article.markdown = req.body.markdown
+        try {
+            article = await article.save()
+            res.redirect(`/articles/${article.slug}`)
+        } catch (err) {
+            res.render(`articles/${path}`, {article: article})
+        }
+    }
+}
 
 module.exports = router 
--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require("express")
 const mongoose = require('mongoose')
 const Article = require("./models/article")
 const articleRouter = require("./routes/articles")
+const methodOverride = require("method-override")
 const app = express()
 
-mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true, useUnifiedTopology: true,
+useCreateIndex: true })
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 
 app.get('/', async (req, res) => {
     const articles = await Article.find().sort({
